49ad6560a2f6a079819908e31c98d78a6fe94663
Augusto Modesto <augusto.modesto@live.com>
2014-06-19 18:12:04 -0300

    Adicionado o agenteNeurologista

---
+++ src/AgenteNeurologista.java
@@ -0,0 +1,53 @@
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+
+
+public class AgenteNeurologista extends Agent {
+	
+	private static final long serialVersionUID = 3464874314236807824L;
+
+	protected void setup() {
+		
+		ServiceDescription servico = new ServiceDescription();
+		servico.setType("Trata problemas neurológicos");
+		servico.setName(this.getLocalName());
+		resgistrarServico(servico);
+		RecebeMensagem("Problema Neurológico", "Vou resolver o problema neurológico do paciente.");
+		
+	}
+
+	protected void resgistrarServico(ServiceDescription sd) {
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.addServices(sd);
+		try{
+			DFService.register(this, dfd);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void RecebeMensagem (final String mensagem, final String resposta) {
+		
+		addBehaviour (new CyclicBehaviour(this) {
+
+			private static final long serialVersionUID = -6793138883957438644L;
+
+			public void action() {
+				ACLMessage msg = receive();
+				if (msg != null) {
+					if (msg.getContent().equalsIgnoreCase(mensagem)) {
+						ACLMessage reply = msg.createReply();
+						reply.setContent(resposta);
+						myAgent.send(reply);
+					}
+				} else 
+					block();
+			}
+		});
+	}
+}
